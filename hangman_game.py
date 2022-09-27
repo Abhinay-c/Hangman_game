03ad1aa923bfaef826a0a888b22364562bb56b29
Abhinay-c <abhinayc365@gmail.com>
2022-09-27 14:28:36 +0530

    random words

--- hangman_game.py
+++ hangman_game.py
@@ -1,21 +1,28 @@
-word = ['h','o','g','w','a','r','t','s']
-guess=['_','_','_','_','_','_','_','_']
-chances = 12
+import random
+names = [list("hogwarts"), list("python"), list("engineering"), list("electrical"), list("btech"), list("spell"), list("wand")]
+word = random.choice(names)
+guess=['_']*len(word)
+chances = len(word)
 b = False
 while (chances > 0 and b == False):
-    print("Your remaining chances are:",chances)
-    print(''.join(guess))
+    print("Chances left:",chances)
+    print(' '.join(guess))
     x = input("Guess a letter: ")
     if x not in word:
         print("Wrong guess")
         chances -= 1
     else:
-        n = len(word)
-        for i in range(n):
-            if (word[i] == x):
-                guess[i] = x
-        if '_' not in guess:
-            b = True
+        if x not in guess:
+            n = len(word)
+            for i in range(n):
+                if (word[i] == x):
+                    guess[i] = x
+            if '_' not in guess:
+                b = True
+        else:
+            print("You re-entered same letter")
 if '_' not in guess:
     print("Your guess is correct!")
     print("The word is",''.join(guess))
+else:
+    print("Failed")
