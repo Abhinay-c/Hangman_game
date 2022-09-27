4d67b66c8d6cd37a75b7e7b2e28cf136fc3dee69
Abhinay-c <abhinayc365@gmail.com>
2022-09-27 13:03:12 +0530

    hangman_game

---
+++ hangman_game.py
@@ -0,0 +1,20 @@
+word = ['h','o','g','w','a','r','t','s','a']
+guess=['_','_','_','_','_','_','_','_','_']
+chances = 12
+b = False
+while (chances > 0 and b == False):
+    print("Your remaining chances are:",chances)
+    print(''.join(guess))
+    x = input("Guess a letter: ")
+    if x not in word:
+        print("Wrong guess")
+        chances -= 1
+    else:
+        for i in range(n):
+            if (word[i] == x):
+                guess[i] = x
+        if '_' not in guess:
+            b = True
+if '_' not in guess:
+    print("Your guess is correct!")
+    print("The word is",''.join(guess))
